8486c6f06373d43937230ffeb828e2419ecfc0ee
Pavlo Rozbytskyi <rozbich15@gmail.com>
2017-02-06 10:45:06 +0100

    Point Class refactoring

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        int x;
+        int y;
+
+        char sym;
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+
+            this.sym = sym;
+        }
+
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
