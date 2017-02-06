bb139ace6097ae19bdb0e283d3bb4f8d00d81240
Pavlo Rozbytskyi <rozbich15@gmail.com>
2017-02-06 16:25:10 +0100

    Created new Snake Class

---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake
+    {
+        List<Point> snakePoints = new List<Point>();
+
+        public Snake(Point beginPoint, int snakeLenght, Direction direction)
+        {
+
+            for (int i = 0; i < snakeLenght; i++)
+            {
+                Point movedPoint = new Point(beginPoint.X, beginPoint.Y, beginPoint.Symbol);
+                //changing location of the point
+                movedPoint.ShiftedPoint(i, direction);
+
+                //adding new point to the list
+                snakePoints.Add(movedPoint);
+            }
+            
+
+
+        }
+
+
+        //drawing method
+        public void DrawSnake()
+        {
+            foreach (Point snakePoint in snakePoints)
+            {
+                Console.Write(snakePoint.Symbol);
+            }
+        }
+    }
+}
