e7416331725c1bc0a537f6f93cc462286e36369e
Pavlo Rozbytskyi <rozbich15@gmail.com>
2017-02-06 13:36:00 +0100

    Created new working HorizontalLine Class

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        //using list of the points
+        List<Point> points;
+
+        public HorizontalLine(List<Point> points)
+        {
+            this.points = points;
+        }
+
+        //Draw method
+        public void DrawHorizontalLine()
+        {
+            Console.SetCursorPosition(points[0].X, points[0].Y);
+
+            foreach (Point point in points)
+            {
+                Console.Write(point.Symbol);
+            }
+        }
+    }
+}
