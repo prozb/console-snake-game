101f24b7e68112480bbb44f36cf2bcf18e2a5f9a
Pavlo Rozbytskyi <rozbich15@gmail.com>
2017-02-07 11:24:54 +0100

    Added new Method to Snake Class, now is our snake moving

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,10 +8,14 @@ namespace Snake
 {
     class Snake : Figure
     {
-        
+        //create direction
+        Direction direction;
+
         public Snake(Point beginPoint, int snakeLenght, Direction direction)
         {
             points = new List<Point>();
+
+            this.direction = direction;
 
             for (int i = 0; i < snakeLenght; i++)
             {
@@ -22,6 +26,36 @@ namespace Snake
                 //adding new point to the list
                 points.Add(movedPoint);
             }
+        }
+
+        internal void Move()
+        {
+            Point endOfSnake = new Point(points[0].X, points[0].Y, points[0].Symbol);
+
+            //clear old symbol *
+            endOfSnake.Clear();
+
+            //remove end of the snake
+            points.RemoveAt(0);
+
+            //create copy of the head
+            Point newHeadOfSnake = GetNextPoint();
+
+            //add new head to the list
+            points.Add(newHeadOfSnake);
+        }
+
+        //new head method
+        public Point GetNextPoint()
+        {
+            Point head = points.Last();
+
+            //copy of the head
+            Point newHeadOfStake = new Point(head.X, head.Y, head.Symbol);
+
+            //new point ahead
+            newHeadOfStake.ShiftedPoint(1, direction);
+            return newHeadOfStake;
         }
     }
 }
